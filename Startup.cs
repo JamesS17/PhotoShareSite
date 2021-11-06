4f7d3d655b77340c07ec794e9a6e5853ef8827a3
JH_Smith <jhsmith7@gmail.com>
2021-11-06 21:44:35 +0200

    A simple photo share VIA Azure and it connects and reads the blobs but upload of photos not going to URL

--- Startup.cs
+++ Startup.cs
@@ -32,8 +32,11 @@ namespace PhotoShareSite
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
             services.AddControllersWithViews();
             services.AddRazorPages();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
