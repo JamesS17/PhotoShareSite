8edba5ed4122cf6ada587c102e7cfd07622e4044
JH_Smith <jhsmith7@gmail.com>
2021-11-09 10:14:44 +0200

    Upload Bug Fix

---
+++ Models/BlobInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoShareSite.Models
+{
+    public class BlobInfo
+    {
+        public string ImageUri { get; set; }
+        public string ThumbnailUri { get; set; }
+        public string Caption { get; set; }
+    }
+}
