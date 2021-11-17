e2361a5eee509adcaeade97ac3fea94e6df67112
HanuEbersohn <hanu.ebersohn@gmail.com>
2021-11-17 21:22:19 +0200

    Added Image Detail Edit feature and Image download feature

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using PhotoShareSite.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -33,24 +37,75 @@ namespace PhotoShareSite.Controllers
         }
 
         [HttpPost]
-       
-    public async Task<IActionResult> UploadNewImage(IFormFile file, string title, string tags, string geoLocation)
-    {
+        public async Task<IActionResult> UploadNewImage(IFormFile file, string title, string tags, string geoLocation)
+        {
             
 
+                var container = _imageService.GetBlobContainer(AzureConnectionString, "photodb");
+            var content = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+            var fileName = content.FileName.Trim('"');
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's userId
+                var userName = User.FindFirstValue(ClaimTypes.Name);
+
+                var blob = container.GetBlockBlobReference(fileName);
+            await blob.UploadFromStreamAsync(file.OpenReadStream());
+            await _imageService.SetImage(title, tags, blob.Uri, userId, userName, geoLocation);
+
+
+
+            return RedirectToAction("Index", "Gallery");
+        }
+
+        public IActionResult EditPhoto(int id)
+        {
+
+            var image = _imageService.GetById(id);
+            string imgT = "*$%";
+            foreach (var item in image.Tags.Select(t => t.Description).ToList())
+            {
+                imgT = imgT+", " + item;
+            }
+            imgT = imgT.Replace("*$%, ", "");
+            var model = new EditPhotoModel()
+            {
+                Id = image.Id,
+                Title = image.Title,
+                Tags = imgT,
+                GeoLocation = image.GeoLocation
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditImage(int id, string title, string tags, string geoLocation)
+        {
+
+
+            await _imageService.EditImage(id, title, tags, geoLocation);
+
+
+
+            return RedirectToAction("Index", "Gallery");
+        }
+
+        public async Task<IActionResult> DeleteImage(int id)
+        {
+            var image = _imageService.GetById(id);
+            Uri uri = new Uri(image.Url);
             var container = _imageService.GetBlobContainer(AzureConnectionString, "photodb");
-        var content = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
-        var fileName = content.FileName.Trim('"');
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's userId
-            var userName = User.FindFirstValue(ClaimTypes.Name);
-
-            var blob = container.GetBlockBlobReference(fileName);
-        await blob.UploadFromStreamAsync(file.OpenReadStream());
-        await _imageService.SetImage(title, tags, blob.Uri, userId, userName, geoLocation);
 
 
+            string filename = Path.GetFileName(uri.LocalPath);
 
-        return RedirectToAction("Index", "Gallery");
+            var blob = container.GetBlockBlobReference(filename);
+
+            await _imageService.DelImage(id);
+            await blob.DeleteIfExistsAsync();
+
+
+
+            return RedirectToAction("Index", "Gallery");
+        }
     }
-}
 }
