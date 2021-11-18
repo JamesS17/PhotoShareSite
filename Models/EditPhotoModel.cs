5c43a319e51a4b3c4e41806064de94e9558f9b33
JH_Smith <jhsmith7@gmail.com>
2021-11-19 00:16:51 +0200

    Tag search fixed. Photos seperated by own and shared

---
+++ Models/EditPhotoModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoShareSite.Models
+{
+    public class EditPhotoModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Tags { get; set; }
+        public string GeoLocation { get; set; }
+    }
+}
